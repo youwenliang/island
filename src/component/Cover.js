2fc6e126068b4d0bf21052b4a6dce84abbd70d1b
Mark Liang <mliang@mozilla.com>
2018-09-04 13:40:19 +0800

    add phone

--- src/component/Cover.js
+++ src/component/Cover.js
@@ -3,8 +3,17 @@ import coverVideo from '../videos/cover.mp4';
 import cover from '../images/cover.svg';
 import timemachine from '../images/timemachine.svg';
 import hand from '../images/hand.svg';
+import phone from '../images/phone.svg';
+import $ from 'jquery';
 
 class Cover extends Component {
+  componentDidMount(){
+    $(document).ready(function(){
+      $('#phone').click(function(){
+        $('.dialog').toggleClass('hiding');
+      })
+    })
+  }
   render() {
     var machineStyle = {
       bottom: "-28px"
@@ -12,6 +21,11 @@ class Cover extends Component {
     var handStyle = {
       bottom: "-20px",
       transform: "translateX(70px)"
+    }
+    var phoneStyle = {
+      bottom: "140px",
+      transform: "translateX(-180px)",
+      pointerEvents: "auto"
     }
     return (
       <section id="cover" className="min-vh-100 bg-gray flex jcc aic">
@@ -32,14 +46,16 @@ class Cover extends Component {
             </video>
           </div>
         </div>
-        <div className="mw8 center ph3-l pv6 z1">
+        <div className="mw8 center ph3-l pv6 z1 pn">
           <div className="cf tc white w-80-ns w-90 center pa4-l">
-            <div className="w-100 mw7 bg-white absolute absolute-center br4 shadow-5 z4 hiding dialog">
-              <p className="f4-ns f5 black pa5-ns pa4 mv0 tl lh-copy">品古不大像親帶不？初體因？結食原的：對使府做設比定不車一響精成子研道法話像風，五機力須讀我；資客家黃總使前？學關見！現成談是我去灣，人德小也樂頭識不包農條，車裡說動我紀集要次她今型團情頭地知人好重得人提山以德頭岸濟只。</p>
+            <div className="w-100 mw7 absolute absolute-center br4 shadow-5 z4 hiding dialog overflow-hidden">
+              <div className="bg-white w-100 h-100 o-90 absolute top0"/>
+              <p className="f4-ns f5 black pa5-ns pa4 mv0 tl lh-copy z1 relative">品古不大像親帶不？初體因？結食原的：對使府做設比定不車一響精成子研道法話像風，五機力須讀我；資客家黃總使前？學關見！現成談是我去灣，人德小也樂頭識不包農條，車裡說動我紀集要次她今型團情頭地知人好重得人提山以德頭岸濟只。</p>
             </div>
             <img src={cover} width="800px" height="auto" alt="title" />
             <img className="absolute absolute-center" style={machineStyle} width="400px" src={timemachine} alt="timemachine"/>
             <img className="absolute absolute-center" style={handStyle} width="135px" src={hand} alt="hand"/>
+            <img id="phone" className="absolute absolute-center cp z10" style={phoneStyle} width="135px" src={phone} alt="phone"/>
           </div>
         </div>
       </section>
